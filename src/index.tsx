224ea417cee37f8d5bce3870cbcba0860cd25a6c
noahgreff <greffnoah@gmail.com>
2023-05-17 15:23:02 +0200

    added `theme`

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './theme/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<ThemeProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	</React.StrictMode>
 )
---
+++ src/theme/theme.ts
@@ -0,0 +1,56 @@
+import { colors, createTheme } from '@mui/material'
+
+export const theme = createTheme({
+	components: {
+		MuiCard: {
+			defaultProps: {
+				variant: 'outlined',
+				style: {
+					borderRadius: 7.5,
+					display: 'inline-block'
+				}
+			}
+		},
+		MuiTooltip: {
+			defaultProps: {
+				disableInteractive: true,
+				arrow: true,
+				placement: 'top'
+			},
+			styleOverrides: {
+				tooltip: {
+					fontSize: 13,
+					fontWeight: 400,
+					backgroundColor: 'black'
+				},
+				arrow: {
+					color: 'black'
+				}
+			}
+		},
+		MuiTypography: {
+			styleOverrides: {
+				h3: {
+					fontSize: 20,
+					fontWeight: 700
+				},
+				h4: {
+					fontSize: 16,
+					fontWeight: 900
+				},
+				caption: {
+					fontSize: 12,
+					color: 'secondary'
+				}
+			}
+		},
+		MuiTableRow: {
+			defaultProps: {
+				hover: true,
+				style: {
+					cursor: 'pointer'
+				}
+			}
+		}
+	}
+})
