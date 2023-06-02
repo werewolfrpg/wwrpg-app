e85e027dd53166d2e12ffeb52079462f9162f5a8
noahgreff <greffnoah@gmail.com>
2023-06-02 08:27:43 +0200

    improved leaderboard ui

--- src/pages/leaderboard/components/leaderboard.tsx
+++ src/pages/leaderboard/components/leaderboard.tsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {
-	Card,
-	Stack,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableFooter,
-	TableHead,
-	TablePagination,
-	TableRow,
-	Typography
-} from '@mui/material'
+import { Box, Card, Divider, Grid, Hidden, Skeleton, Stack, TablePagination, Typography } from '@mui/material'
 import { Leaderboard } from '../../../types/leaderboard'
 import { getLeaderboard } from '../../../apis/wwrpg'
 
 export default () => {
 	const navigate = useNavigate()
-
 	const [page, setPage] = useState(0)
 	const [count, setCount] = useState(20)
 	const [leaderboard, setLeaderboard] = useState<Leaderboard | null>(null)
@@ -35,60 +22,111 @@ export default () => {
 	}
 
 	if (!leaderboard) {
-		return <div>loading...</div>
+		const skeletons = []
+		for (let i = 0; i < 3; i++) {
+			skeletons.push(
+				<Row
+					light={i % 2 === 0}
+					values={[
+						<Skeleton width={20} />,
+						<Stack direction="row" alignItems="center" gap={2}>
+							<Skeleton width={40} height={40} variant="circular" />
+							<Skeleton width={100} />
+						</Stack>,
+						<Skeleton width={60} />,
+						<Skeleton width={40} />,
+						<Skeleton width={40} />,
+						<Skeleton width={30} />
+					]}
+				/>
+			)
+		}
+
+		return (
+			<Card>
+				<Row values={['#', 'Player', 'Title', 'Score', 'Win %', 'Games Played']} />
+				{skeletons}
+				<Row values={[<Skeleton width={120} />]} />
+			</Card>
+		)
 	}
 
 	return (
-		<TableContainer component={Card}>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell align="left">Rank</TableCell>
-						<TableCell align="left">Player</TableCell>
-						<TableCell align="center">Title</TableCell>
-						<TableCell align="center">Score</TableCell>
-						<TableCell align="center">Win %</TableCell>
-						<TableCell align="right">Matches Played</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{leaderboard.data.map(({ minecraftId, rank, username, title, score, won, played }) => (
-						<TableRow
-							hover
-							style={{ cursor: 'pointer' }}
-							key={minecraftId}
-							onClick={() => navigate('/overview/player/' + minecraftId)}
-						>
-							<TableCell align="left">#{rank}</TableCell>
-							<TableCell align="left">
-								<Stack direction="row" alignItems="center">
-									<img src={'https://mc-heads.net/head/' + minecraftId} height={40} />
-									<Typography fontWeight={900} p={2}>
-										{username}
-									</Typography>
-								</Stack>
-							</TableCell>
-							<TableCell align="center">{title}</TableCell>
-							<TableCell align="center">{score}</TableCell>
-							<TableCell align="center">{played > 0 ? ((won / played) * 100).toFixed(1) + '%' : '--'}</TableCell>
-							<TableCell align="right">{played}</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-				<TableFooter>
-					<TableRow>
-						<TablePagination
-							style={{ border: 'none' }}
-							rowsPerPageOptions={[20, 50, 100]}
-							onPageChange={(_, page) => refreshLeaderboard(page, count)}
-							onRowsPerPageChange={event => refreshLeaderboard(0, parseInt(event.target.value))}
-							count={leaderboard.meta.entries * leaderboard.meta.totalPageNumber}
-							rowsPerPage={count}
-							page={page}
-						/>
-					</TableRow>
-				</TableFooter>
-			</Table>
-		</TableContainer>
+		<Card>
+			<Row values={['#', 'Player', 'Title', 'Score', 'Win %', 'Games Played']} />
+			{leaderboard.data.map((player, index) => (
+				<Row
+					onClick={() => navigate('/overview/player/' + player.minecraftId)}
+					key={index}
+					light={index % 2 === 0}
+					values={[
+						player.rank,
+						<Stack direction="row" alignItems="center" gap={2}>
+							<Box component="img" src={'https://mc-heads.net/head/' + player.minecraftId} height={40} />
+							<Divider style={{ color: 'white' }} orientation="vertical" />
+							<Typography variant="h4">{player.username}</Typography>
+						</Stack>,
+						player.title,
+						player.score,
+						player.played > 0 ? ((player.won / player.played) * 100).toFixed(1) + '%' : '--',
+						player.played
+					]}
+				/>
+			))}
+			<TablePagination
+				style={{ border: 'none' }}
+				rowsPerPageOptions={[20, 50, 100]}
+				onPageChange={(_, page) => refreshLeaderboard(page, count)}
+				onRowsPerPageChange={event => refreshLeaderboard(0, parseInt(event.target.value))}
+				count={leaderboard.meta.entries * leaderboard.meta.totalPageNumber}
+				rowsPerPage={count}
+				page={page}
+			/>
+		</Card>
+	)
+}
+
+const Row = ({
+	values,
+	light = false,
+	onClick
+}: {
+	values: (string | React.ReactNode)[]
+	light?: boolean
+	onClick?: () => unknown
+}) => {
+	const [rank, player, title, score, win, matches] = values
+
+	return (
+		<Grid
+			container
+			onClick={onClick}
+			style={{ cursor: onClick ? 'pointer' : 'inherit' }}
+			bgcolor={light ? 'background.default' : 'background.paper'}
+			p={2}
+		>
+			<Grid item justifySelf="center" alignSelf="center" xs={1}>
+				{rank}
+			</Grid>
+			<Grid item justifySelf="center" alignSelf="center" xs>
+				{player}
+			</Grid>
+			<Hidden smDown>
+				<Grid item justifySelf="center" alignSelf="center" xs={2}>
+					<Stack alignItems="center">{title}</Stack>
+				</Grid>
+				<Grid item justifySelf="center" alignSelf="center" xs={1}>
+					<Stack alignItems="center">{score}</Stack>
+				</Grid>
+			</Hidden>
+			<Grid item justifySelf="center" alignSelf="center" xs={1}>
+				<Stack alignItems="center">{win}</Stack>
+			</Grid>
+			<Hidden smDown>
+				<Grid item justifySelf="center" alignSelf="center" xs={2}>
+					<Stack alignItems="end">{matches}</Stack>
+				</Grid>
+			</Hidden>
+		</Grid>
 	)
 }
